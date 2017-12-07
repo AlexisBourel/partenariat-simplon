b7a1638b5c29ac3b11356bd7bd6bf8b5b60360e7
AlexisBourel <alexisbourel.pro@gmail.com>
2017-12-07 17:35:29 +0100

    Ajout du plugin

--- README.md
+++ README.md
@@ -1,17 +1,17 @@
-# Partenariat Simplon
+﻿# Partenariat Simplon
 ## Une Simple Extention WordPress
 
 ### Description :
 
-...
+Un simple short code pour ajouter du texte sous Wordpress ...
 
 ### Installation :
 
-...
+Zip le dossier puis aller dans : Tableau de bord Extention Ajouter Ajouter le zip ...
 
 ### Utilisation :
 
-...
+Ajouter [simplon] ou vous désirez ajouter votre texte. ...
 
 ### Ressources :
 #### Déclaration du plugin :
--- partenariat-simplon.php
+++ partenariat-simplon.php
@@ -1,10 +1,13 @@
 <?php
 /*
-
+Description: Un plugin ajoutant du texte sous WordPress
+Version: 0.1
+Author: Alexis BOUREL
 */
-
-
-
-
-
+function SimplonShortcode() {
+	return '<p>La publication de cet article est possible grâce à mon super partenaire <a href="https://simplon.co">simplon.co</a> - une entreprise de
+	l’économie sociale et solidaire et un réseau de « fabriques » (écoles) qui propose
+	des formations GRATUITES pour devenir développeur web.</p>';
+	}
+add_shortcode('simplon', 'SimplonShortcode');
 ?>
